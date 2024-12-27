3f19fe1bac27874d34d092e96043799c7f81bbcb
IvanPerez <ivan.perez96@educa.madrid.org>
2024-12-27 11:02:53 +0100

    Creada funcion en UsuarioRepository para permitirme filtrar por correo

--- src/main/java/com/troncoPruebas/classroom/controllers/UsuarioController.java
+++ src/main/java/com/troncoPruebas/classroom/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.troncoPruebas.classroom.controllers;
 
 import com.troncoPruebas.classroom.models.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +12,35 @@ import com.troncoPruebas.classroom.repositories.UsuarioRepository;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "api/usuarios")
 public class UsuarioController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
     @Autowired
     private UsuarioRepository usuarioRepository;
     //Si no existe en la intefaz usuarioRepositorio alguna funcion especifica se puede crear en dicho repositorio de manera manual
 
+    @CrossOrigin
+    @PostMapping("/logIn")
+    public ResponseEntity<Usuario> iniciarSesion(@RequestBody Map<String, String> campos) throws NullPointerException {
+        String correo = campos.get("correo");
+        String pass = campos.get("pass");
+        Usuario usuario = usuarioRepository.findByCorreo(correo);
+
+        if (usuario == null) {
+            logger.error("Usuario no encontrado con correo: " + correo);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // Retorna un 404 si el usuario no existe
+        }
+        if(correo.equals(usuario.getCorreo()) && pass.equals(usuario.getContrasena())) {
+            return ResponseEntity.ok().body(usuario);
+        }
+        logger.error("el usuario es null");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
     //EL croosorigin es para cuando por ejemplo el front esta desarollado de manera externa
     @CrossOrigin
     @GetMapping("/all")
