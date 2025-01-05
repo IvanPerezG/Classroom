82f31c5db8472795fbadcce9910b7a5fe5071103
IvanPerez <ivan.perez96@educa.madrid.org>
2025-01-05 09:42:10 +0100

    Modificada entidad Unidad, añadida funcion buscarporAsiganturaId e implementada

--- src/main/java/com/troncoPruebas/classroom/repositories/UnidadRepository.java
+++ src/main/java/com/troncoPruebas/classroom/repositories/UnidadRepository.java
@@ -3,6 +3,8 @@ package com.troncoPruebas.classroom.repositories;
 import com.troncoPruebas.classroom.models.Unidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UnidadRepository  extends JpaRepository<Unidad, Integer> {
-    Unidad findByAsignaturaId(Integer asignaturaId);
+   List<Unidad> findByAsignaturaId(Integer asignaturaId);
 }
