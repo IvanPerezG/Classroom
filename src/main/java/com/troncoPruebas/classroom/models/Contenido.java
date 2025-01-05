c3a1410fcac33a40f0f0fa03cda8fabafd000c9f
IvanPerezG <158555380+IvanPerezG@users.noreply.github.com>
2025-01-05 20:44:48 +0100

    Add files via upload
    añadidas entidades contenido entrega_alumno

---
+++ src/main/java/com/troncoPruebas/classroom/models/Contenido.java
@@ -0,0 +1,25 @@
+package com.troncoPruebas.classroom.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "contenido")
+public class Contenido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String titulo;
+    @Column(name = "unidad_id")
+    private Integer unidadId;
+    @Column(name = "fecha_creacion")
+    private Timestamp fechaCreacion;
+    private String key;
+    private String descripcion;
+}
