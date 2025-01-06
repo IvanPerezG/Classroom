1a1ea13ba26c37f83cc7f94799b14ae9e2ec41c2
IvanPerez <ivan.perez96@educa.madrid.org>
2025-01-06 20:52:27 +0100

    combiado metodo para devolver una lista

--- src/main/java/com/troncoPruebas/classroom/controllers/UnidadController.java
+++ src/main/java/com/troncoPruebas/classroom/controllers/UnidadController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,8 +21,8 @@ public class UnidadController {
 
 
     @CrossOrigin
-    @GetMapping("/unidadAsigantura{id}")
-    public Unidad getUnidadAlumnoId(@PathVariable Integer id) {
+    @GetMapping("/unidadAsignatura{id}")
+    public List<Unidad> getUnidadAsignaturaId(@PathVariable Integer id) {
         return unidadRepository.findByAsignaturaId(id);
     }
 }
