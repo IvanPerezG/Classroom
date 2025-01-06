df525929fd7273048b0d180e04bc2c7c852bd074
IvanPerez <ivan.perez96@educa.madrid.org>
2025-01-06 20:50:53 +0100

    Añadidas funciones para recuperar Usuario y para regsitrar usuario

--- src/main/java/com/troncoPruebas/classroom/controllers/Usuario_AsignaturaController.java
+++ src/main/java/com/troncoPruebas/classroom/controllers/Usuario_AsignaturaController.java
@@ -5,8 +5,11 @@ import com.troncoPruebas.classroom.repositories.Usuario_AsignaturaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/usuarioAsignatura")
@@ -17,13 +20,20 @@ public class Usuario_AsignaturaController {
 
     @CrossOrigin
     @GetMapping("/usuario{id}")
-    public ResponseEntity<Usuario_Asignatura> getAsignatura(@PathVariable Integer id) {
-        Usuario_Asignatura usuarioAsignatura = usuarioAsignaturaRepository.findByUsuarioId(id);
-        if(usuarioAsignatura == null) {
-            return ResponseEntity.notFound().build();
-        }
-        logger.info(usuarioAsignatura.toString());
-        return ResponseEntity.ok(usuarioAsignatura);
+    public List<Usuario_Asignatura> getAsignatura(@PathVariable Integer id) {
+        return  usuarioAsignaturaRepository.findByUsuarioId(id);
+    }
+    @CrossOrigin
+    @GetMapping("/usuarioAsignatura{id}")
+    public List<Usuario_Asignatura> getUsuarios(@PathVariable Integer id) {
+        return  usuarioAsignaturaRepository.findByAsignaturaId(id);
+    }
+    @CrossOrigin
+    @PostMapping("/registrarUsuario")
+    public ResponseEntity<Usuario_Asignatura> registrarUsuario(@RequestBody Usuario_Asignatura usuarioAsignatura) {
+        logger.info("Registrando Usuarioasignatura " +usuarioAsignatura.getusuarioId() +"//"+ usuarioAsignatura.getasignaturaId());
+        Usuario_Asignatura nuevoUsuarioAsignatura =  usuarioAsignaturaRepository.save(usuarioAsignatura);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoUsuarioAsignatura);
     }
 
 }
--- src/main/java/com/troncoPruebas/classroom/repositories/Usuario_AsignaturaRepository.java
+++ src/main/java/com/troncoPruebas/classroom/repositories/Usuario_AsignaturaRepository.java
@@ -3,6 +3,10 @@ package com.troncoPruebas.classroom.repositories;
 import com.troncoPruebas.classroom.models.Usuario_Asignatura;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Usuario_AsignaturaRepository extends JpaRepository<Usuario_Asignatura, Integer> {
-    Usuario_Asignatura findByUsuarioId(Integer id);
+    List<Usuario_Asignatura> findByUsuarioId(Integer id);
+
+    List<Usuario_Asignatura> findByAsignaturaId(Integer id);
 }
